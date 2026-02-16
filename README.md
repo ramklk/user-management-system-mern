a6a16dbff65e884311d29e135f6adb13847d861c
ramklk <ramprasadvkulkarni@gmail.com>
2026-02-16 10:18:12 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Basic Node.js CRUD API
+
+This project demonstrates how to:
+
+- Connect Node.js to MongoDB Atlas
+- Use Mongoose for schema modeling
+- Implement CRUD operations
+- Structure backend using MVC
+- Test APIs using Postman
+
+## Tech Stack
+- Node.js
+- Express
+- MongoDB Atlas
+- Mongoose
+
+## Run Project
+
+1. Clone repository
+2. Run `npm install`
+3. Create `.env` file
+4. Run `npm start`
---
+++ src/controllers/userController.js
@@ -0,0 +1,68 @@
+const User = require("../models/userModel");
+
+
+// 🔹 Create User
+exports.createUser = async (req, res) => {
+  try {
+    const user = await User.create(req.body);
+    res.status(201).json(user);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+
+// 🔹 Get All Users
+exports.getUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+
+// 🔹 Get Single User
+exports.getUserById = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) return res.status(404).json({ message: "User not found" });
+
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+
+// 🔹 Update User
+exports.updateUser = async (req, res) => {
+  try {
+    const user = await User.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true, runValidators: true }
+    );
+
+    if (!user) return res.status(404).json({ message: "User not found" });
+
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+
+// 🔹 Delete User
+exports.deleteUser = async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+
+    if (!user) return res.status(404).json({ message: "User not found" });
+
+    res.status(200).json({ message: "User deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
